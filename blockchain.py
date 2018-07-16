4bfe0e8deef6b3a761856a0ca0deb94ff507a35b
shukubota <kubotashu0421@gmail.com>
2018-07-16 19:19:08 +0900

    アルゴリズムここまで

--- blockchain.py
+++ blockchain.py
@@ -1,6 +1,7 @@
 import hashlib
 import json
 from time import time
+from uuid import uuid4
 
 
 class Blockchain(object):
@@ -10,7 +11,20 @@ class Blockchain(object):
 
     def new_block(self):
         # create block  add chain
-        pass
+
+
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
+
+        # reset current transaction
+        self.current_transactions = []
+
+        self.chain.append(block)
+        return block
 
     def new_transaction(self, sender, recipient, amount):
         # add new transaction
@@ -22,12 +36,28 @@ class Blockchain(object):
 
         return self.last_block['index'] + 1
 
+
+    def proof_of_work(self, last_proof):
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+
+        return proof
+
     @staticmethod
+    def valid_proof(last_proof, proof):
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
+        
     def hash(block):
         #hashed a block
-        pass
+        # create SHA-256 hash of a block
+        #
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
 
     @property
     def last_block(self):
         # return the last block in the chain
-        pass
+        return self.chain[-1]
